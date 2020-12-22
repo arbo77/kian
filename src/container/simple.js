a795ae3c1748a74356be015670394cdd28176b8a
Ari <arbo77@gmail.com>
2020-12-22 19:31:04 +0700

    gap for simple list

--- src/container/simple.js
+++ src/container/simple.js
@@ -4,10 +4,12 @@ import styled from 'styled-components'
 const SimpleTag = styled.ol`
   list-style: none;
   padding: 0;
+  display: grid;
+  gap: ${(props) => props.gap || '1rem'};
 `
-export const Simple = ({ items, component, style, className }) => {
+export const Simple = ({ items, component, style, className, gap }) => {
   return (
-    <SimpleTag style={style} className={className}>
+    <SimpleTag style={style} className={className} gap={gap}>
       {items.map((el, idx) => (
         <li key={idx}>{component({ item: el })}</li>
       ))}
